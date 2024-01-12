9fabf40e88b27fe5e684b7dd7131d7ed4b1b101b
unkmonster <106185616+unkmonster@users.noreply.github.com>
2024-01-12 17:12:10 +0800

    replace 'CreateThread' to 'std::thread' in DllMain

--- Eagle/dllmain.cpp
+++ Eagle/dllmain.cpp
@@ -1,48 +1,37 @@
 #include <Windows.h>
 
 #include <thread>
-#include <chrono>
-#include <future>
-
-#include <MinHook.h>
 
 #include "common.h"
 #include "Global.h"
-#include "utils/Utility.h"
 #include "Utils/Singleton.h"
 #include "Hooking/HookManager.h"
 #include "Logger.h"
 #include "Pointers.h"
-#include <render/Renderer.h>
-
-using namespace std::chrono_literals;
-
-DWORD Launcher(LPVOID param) {
-	try {
-		gLogger = Singleton<Logger>::initialize();
-		gPointers = Singleton<Pointers>::initialize();
-		gRenderer = Singleton<Renderer>::initialize();
-		gHookManager = Singleton<HookManager>::initialize();
-		global.m_running = true;
-		return 0;
-		/*while (global.m_running)
-			std::this_thread::sleep_for(1ms);*/
-	} catch (std::runtime_error& err) {
-		MessageBoxA(
-			NULL, 
-			fmt::format("Launch Error\n{}", err.what()).c_str(), 
-			PROJECTNAME, MB_ICONWARNING);
-		return 1;
-	}
-}
+#include "render/Renderer.h"
 
 BOOL WINAPI DllMain(HMODULE hModule, DWORD reason, LPVOID lpvReserved) {
 	if (reason == DLL_PROCESS_ATTACH) {
 		DisableThreadLibraryCalls(hModule);
 		global.m_thisModule = hModule; // TODO
 
-		if (CreateThread(nullptr, NULL, Launcher, nullptr, 0, nullptr) == NULL) {
-			MessageBoxA(NULL, "Failed to create launch thread!", PROJECTNAME, MB_ICONERROR);
+		try {
+			std::thread([]() {
+				try {
+					gLogger = Singleton<Logger>::initialize();
+					gPointers = Singleton<Pointers>::initialize();
+					gRenderer = Singleton<Renderer>::initialize();
+					gHookManager = Singleton<HookManager>::initialize();
+					global.m_running = true;
+				} catch (std::runtime_error& err) {
+					MessageBoxA(
+						NULL,
+						fmt::format("Launch Error\n{}", err.what()).c_str(),
+						PROJECTNAME, MB_ICONWARNING);
+				}
+			}).detach();
+		} catch (const std::exception& ex) {
+			MessageBoxA(NULL, ex.what(), PROJECTNAME, MB_ICONERROR);
 			return FALSE;
 		}
 	} else if (reason == DLL_PROCESS_DETACH) {
